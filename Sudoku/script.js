f8191c4c8cb2ecd4a97bf5fa83a74fec7ba739f4
Subathra2005 <158835209+Subathra2005@users.noreply.github.com>
2024-07-28 17:27:19 +0530

    Update script.js

--- Sudoku/script.js
+++ Sudoku/script.js
@@ -1,247 +1,272 @@
-document
-  .getElementById("sudoku-board")
-  .addEventListener("keyup", function (event) {
-    if (event.target && event.target.nodeName == "TD") {
-      var validNum = /[1-9]/;
-      var tdEl = event.target;
-      if (tdEl.innerText.length > 0 && validNum.test(tdEl.innerText[0])) {
-        tdEl.innerText = tdEl.innerText[0];
-      } else {
-        tdEl.innerText = "";
-      }
-    }
-  });
-
-document
-  .getElementById("solve-button")
-  .addEventListener("click", function (event) {
-    var boardString = boardToString();
-    var solution = SudokuSolver.solve(boardString);
-    if (solution) {
-      stringToBoard(solution);
-    } else {
-      alert("Invalid board!");
-    }
-  });
-
-document.getElementById("clear-button").addEventListener("click", clearBoard);
-
-function clearBoard() {
-  var tds = document.getElementsByTagName("td");
-  for (var i = 0; i < tds.length; i++) {
-    tds[i].innerText = "";
-  }
+count=0
+for (var i=0;i<9;i++){
+    count=9*i;
+    document.getElementsByClassName("box")[i].innerHTML="<div class='cell'><input type='text' id='"+(count+1)+"' class='input'></div><div class='cell'><input type='text' id='"+(count+2)+"' class='input'></div><div class='cell'><input type='text' id='"+(count+3)+"' class='input'></div><div class='cell'><input type='text' id='"+(count+4)+"' class='input'></div><div class='cell'><input type='text' id='"+(count+5)+"' class='input'></div><div class='cell'><input type='text' id='"+(count+6)+"' class='input'></div><div class='cell'><input type='text' id='"+(count+7)+"' class='input'></div><div class='cell'><input type='text' id='"+(count+8)+"' class='input'></div><div class='cell'><input type='text' id='"+(count+9)+"' class='input'></div>"
 }
 
-function boardToString() {
-  var string = "";
-  var validNum = /[1-9]/;
-  var tds = document.getElementsByTagName("td");
-  for (var i = 0; i < tds.length; i++) {
-    if (validNum.test(tds[i].innerText[0])) {
-      string += tds[i].innerText;
-    } else {
-      string += "-";
-    }
-  }
-  return string;
+//how to play game instruction
+
+function help(){
+    window.open(
+        "https://sudoku.com/how-to-play/sudoku-rules-for-complete-beginners/", "_blank");
 }
+var level;
+var choosen;
 
-function stringToBoard(string) {
-  var currentCell;
-  var validNum = /[1-9]/;
-  var cells = string.split("");
-  var tds = document.getElementsByTagName("td");
-  for (var i = 0; i < tds.length; i++) {
-    currentCell = cells.shift();
-    if (validNum.test(currentCell)) {
-      tds[i].innerText = currentCell;
+
+// easy_level board create
+
+easy_board=['2-5---7--45---9----2-6-81----9---8567-------2418---2----43-7-1----1---85--6---7-8','----35-86-1-9-7-----269----54------------527-9--75----7-6---3-----2-----56---2-14','3-549-6----396--81-5-2--1494-276-1-39---583-46-1549--7-6-1-824558-7-3-924---7-3-6','47----3-------179--4-93--5----6---7-48---------2716-34-9----6----6--2381---54--1-','-2--18573-31--5-96---16----5--4-26--97--86--------98--1-6--79--2-5---8144-9-7---1'
+];
+
+easy=['215986734452869371527648193379124856781543692418937265864357219693172485936521748','129735486213967854342691578543869127498315276981754632786421359675248193567832914','315492678723964581857236149482765193916258374631549827967138245584713692429871356','476285319523861794148932657123649578481397265952716834895137624976452381763548219','429618573831245796382164957537492618974186325761259843186537942265793814459378621'];
+
+//medium level board create
+
+medium=['876345291982754163417638529493712568135826947359271684251968473746319825682594137','834615279152793468921546387512879346634258791463987125796342581978614235857123469','695138472243761589817356924428975361796854213532149687137264598851923476649782315','184936257623498715372561849259847136491375682514928763673521894758162439986347215','439671258825764193316582947612583497741239865924176358578429613396185742857934261'];
+
+medium_board=['--6----9---75-1---1------9-9-7-25-8-3-----4-3-92-1-8-2------7---6-19--5-8----1--','------27----793--892-5-63--5--87-3---34-5-79---3-87--5--63-2-819--614----57------','6-5-384--2----1--9-1----9-------53--7--8-4--3--21-------7----9-8--9----6--978-3-5','-8493--576--4----5---------2--84----4-1-7-6-2----28--3---------7----2--998--4721-','43--7-2---2----1-----5--9-------349-741---865-241-------8--9-----6----4---7-3--61'];
+
+//hard level board create
+
+hard_board=['---789-----75-8-4---38-----8---1---6---7-9---2---7---1-----61---5-3-42-----439---','-6------2---9-83----6--3-79----368---2-----4---461----75-8--4----51-7---2------8-','-8-------4---15--3---69-----2-73-1----9-----2-6----------19--875--9-2-1--2-835---','----578------3--19---3---75-5-2-8--4------6---1-7-24---7----6----142---3---9--3--','--346-5-------------9-4-8--5-9---18----4-3--7-------7--4--------9--81--6-3-71----'];
+
+hard=['165789432297518346973821654847312596463729158284675931923546178851364297615439782','861794532617948325156283479492536871928365147784619253753812496345127968239574681','187256349428715963534691872924738156619483572861247395356194287573962418729835641','143657892526738419984361275956238174843591627315782469872149635791426583267954318','783461592318627954179246835569732184625493817246358971142958673497581326835719264'];
+
+
+function start(){
+    for(var i=0;i<6;i++){
+        document.getElementsByClassName("label")[i].setAttribute("onclick","return false;");
     }
-  }
-}
-
-("use strict");
-
-var EASY_PUZZLE =
-  "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--";
-var MEDIUM_PUZZLE =
-  "-3-5--8-45-42---1---8--9---79-8-61-3-----54---5------78-----7-2---7-46--61-3--5--";
-var HARD_PUZZLE =
-  "8----------36------7--9-2---5---7-------457-----1---3---1----68--85---1--9----4--";
-
-var TESTABLE = true;
-
-var SudokuSolver = (function (testable) {
-  var solver;
-
-  function solve(boardString) {
-    var boardArray = boardString.split("");
-    if (boardIsInvalid(boardArray)) {
-      return false;
-    }
-    return recursiveSolve(boardString);
-  }
-
-  function solveAndPrint(boardString) {
-    var solvedBoard = solve(boardString);
-    console.log(toString(solvedBoard.split("")));
-    return solvedBoard;
-  }
-
-  function recursiveSolve(boardString) {
-    var boardArray = boardString.split("");
-    if (boardIsSolved(boardArray)) {
-      return boardArray.join("");
-    }
-    var cellPossibilities = getNextCellAndPossibilities(boardArray);
-    var nextUnsolvedCellIndex = cellPossibilities.index;
-    var possibilities = cellPossibilities.choices;
-    for (var i = 0; i < possibilities.length; i++) {
-      boardArray[nextUnsolvedCellIndex] = possibilities[i];
-      var solvedBoard = recursiveSolve(boardArray.join(""));
-      if (solvedBoard) {
-        return solvedBoard;
-      }
-    }
-    return false;
-  }
-
-  function boardIsInvalid(boardArray) {
-    return !boardIsValid(boardArray);
-  }
-
-  function boardIsValid(boardArray) {
-    return (
-      allRowsValid(boardArray) &&
-      allColumnsValid(boardArray) &&
-      allBoxesValid(boardArray)
-    );
-  }
-
-  function boardIsSolved(boardArray) {
-    for (var i = 0; i < boardArray.length; i++) {
-      if (boardArray[i] === "-") {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  function getNextCellAndPossibilities(boardArray) {
-    for (var i = 0; i < boardArray.length; i++) {
-      if (boardArray[i] === "-") {
-        var existingValues = getAllIntersections(boardArray, i);
-        var choices = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].filter(
-          function (num) {
-            return existingValues.indexOf(num) < 0;
-          }
-        );
-        return { index: i, choices: choices };
-      }
-    }
-  }
-
-  function getAllIntersections(boardArray, i) {
-    return getRow(boardArray, i)
-      .concat(getColumn(boardArray, i))
-      .concat(getBox(boardArray, i));
-  }
-
-  function allRowsValid(boardArray) {
-    return [0, 9, 18, 27, 36, 45, 54, 63, 72]
-      .map(function (i) {
-        return getRow(boardArray, i);
-      })
-      .reduce(function (validity, row) {
-        return collectionIsValid(row) && validity;
-      }, true);
-  }
-
-  function getRow(boardArray, i) {
-    var startingEl = Math.floor(i / 9) * 9;
-    return boardArray.slice(startingEl, startingEl + 9);
-  }
-
-  function allColumnsValid(boardArray) {
-    return [0, 1, 2, 3, 4, 5, 6, 7, 8]
-      .map(function (i) {
-        return getColumn(boardArray, i);
-      })
-      .reduce(function (validity, row) {
-        return collectionIsValid(row) && validity;
-      }, true);
-  }
-
-  function getColumn(boardArray, i) {
-    var startingEl = Math.floor(i % 9);
-    return [0, 1, 2, 3, 4, 5, 6, 7, 8].map(function (num) {
-      return boardArray[startingEl + num * 9];
-    });
-  }
-
-  function allBoxesValid(boardArray) {
-    return [0, 3, 6, 27, 30, 33, 54, 57, 60]
-      .map(function (i) {
-        return getBox(boardArray, i);
-      })
-      .reduce(function (validity, row) {
-        return collectionIsValid(row) && validity;
-      }, true);
-  }
-
-  function getBox(boardArray, i) {
-    var boxCol = Math.floor(i / 3) % 3;
-    var boxRow = Math.floor(i / 27);
-    var startingIndex = boxCol * 3 + boxRow * 27;
-    return [0, 1, 2, 9, 10, 11, 18, 19, 20].map(function (num) {
-      return boardArray[startingIndex + num];
-    });
-  }
-
-  function collectionIsValid(collection) {
-    var numCounts = {};
-    for (var i = 0; i < collection.length; i++) {
-      if (collection[i] != "-") {
-        if (numCounts[collection[i]] === undefined) {
-          numCounts[collection[i]] = 1;
-        } else {
-          return false;
+    timer();
+    //easy game
+    if(document.getElementById("easy").checked){
+        level='easy';
+        var easy_random=Math.floor(Math.random()*5);
+        choosen=easy_random;
+        for(var i=0;i<81;i++){
+            if(easy_board[easy_random][i]!='-'){
+                document.getElementById((i+1).toString()).value=easy_board[easy_random][i];
+                document.getElementById((i+1).toString()).readOnly=true;
+            }
         }
-      }
     }
-    return true;
-  }
 
-  function toString(boardArray) {
-    return [0, 9, 18, 27, 36, 45, 54, 63, 72]
-      .map(function (i) {
-        return getRow(boardArray, i).join(" ");
-      })
-      .join("\n");
-  }
+    //medium game
 
-  if (testable) {
-    solver = {
-      solve: solve,
-      solveAndPrint: solveAndPrint,
-      recursiveSolve: recursiveSolve,
-      boardIsInvalid: boardIsInvalid,
-      boardIsValid: boardIsValid,
-      boardIsSolved: boardIsSolved,
-      getNextCellAndPossibilities: getNextCellAndPossibilities,
-      getAllIntersections: getAllIntersections,
-      allRowsValid: allRowsValid,
-      getRow: getRow,
-      allColumnsValid: allColumnsValid,
-      getColumn: getColumn,
-      allBoxesValid: allBoxesValid,
-      getBox: getBox,
-      collectionIsValid: collectionIsValid,
-      toString: toString,
-    };
-  } else {
-    solver = { solve: solve, solveAndPrint: solveAndPrint };
-  }
+    else if(document.getElementById("medium").checked){
+        level='medium';
+        var medium_random=Math.floor(Math.random()*5);
+        choosen=medium_random;
+        for(var i=0;i<81;i++){
+            if(medium_board[medium_random][i]!='-'){
+                document.getElementById((i+1).toString()).value=medium_board[medium_random][i];
+                document.getElementById((i+1).toString()).readOnly=true;
+            }
+        }
+    }
 
-  return solver;
-})(TESTABLE);
+
+//hard game
+
+else{
+    level='hard';
+        var hard_random=Math.floor(Math.random()*5);
+        choosen=hard_random;
+        for(var i=0;i<81;i++){
+            if(hard_board[hard_random][i]!='-'){
+                document.getElementById((i+1).toString()).value=hard_board[hard_random][i];
+                document.getElementById((i+1).toString()).readOnly=true;
+            }
+        }
+}
+
+
+document.getElementById("start").removeAttribute("onclick");
+   
+}
+
+
+//check answer
+var id=setInterval(() => {
+    if (level=="easy"){
+    if(document.activeElement.className=="input"){
+        if((document.getElementById(document.activeElement.id).value==easy[choosen][document.activeElement.id-1])||(document.getElementById(document.activeElement.id).value=='')){
+            for(var i=0;i<81;i++){
+                if(i==80 && document.getElementById((81).toString()).value!='' ){
+                        alert("you win !! congratulation.....");
+                        clearInterval(id);
+                        window.location.reload();
+                }
+                else if(document.getElementById((i+1).toString()).value==''){
+                    break;
+                }
+            }
+        }
+        else{
+            if(document.getElementById("rem_live").innerHTML==1){
+                document.getElementById("rem_live").innerHTML==0;
+                alert("you lost !!");
+                document.activeElement.value='';
+                window.location.reload();
+
+            }
+            else{
+            alert("you choose wrong number, you loose your one life !!");
+            document.getElementById("rem_live").innerHTML=document.getElementById("rem_live").innerHTML-1;
+            document.activeElement.value='';
+            }
+        }
+
+    }
+}
+
+else if(level=="medium"){
+
+    if(document.activeElement.className=="input"){
+        if((document.getElementById(document.activeElement.id).value==medium[choosen][document.activeElement.id-1])||(document.getElementById(document.activeElement.id).value=='')){
+            for(var i=0;i<81;i++){
+                if(i==80 && document.getElementById((81).toString()).value!='' ){
+                        alert("you win !! congratulation.....");
+                        clearInterval(id);
+                        window.location.reload();
+                }
+                else if(document.getElementById((i+1).toString()).value==''){
+                    break;
+                }
+            }
+        }
+        else{
+            if(document.getElementById("rem_live").innerHTML==1){
+                document.getElementById("rem_live").innerHTML==0;
+                alert("you lost !!");
+                document.activeElement.value='';
+                window.location.reload();
+
+            }
+            else{
+            alert("you choose wrong number, you loose your one life !!");
+            document.getElementById("rem_live").innerHTML=document.getElementById("rem_live").innerHTML-1;
+            document.activeElement.value='';
+            }
+        }
+
+    }
+}
+
+else{
+
+    if(document.activeElement.className=="input"){
+        if((document.getElementById(document.activeElement.id).value==hard[choosen][document.activeElement.id-1])||(document.getElementById(document.activeElement.id).value=='')){
+            for(var i=0;i<81;i++){
+                if(i==80 && document.getElementById((81).toString()).value!='' ){
+                        alert("you win !! congratulation.....");
+                        clearInterval(id);
+                        window.location.reload();
+                }
+                else if(document.getElementById((i+1).toString()).value==''){
+                    break;
+                }
+            }
+        }
+        else{
+            if(document.getElementById("rem_live").innerHTML==1){
+                document.getElementById("rem_live").innerHTML==0;
+                alert("you lost !!");
+                document.activeElement.value='';
+                window.location.reload();
+
+            }
+            else{
+            alert("you choose wrong number, you loose your one life !!");
+            document.getElementById("rem_live").innerHTML=document.getElementById("rem_live").innerHTML-1;
+            document.activeElement.value='';
+            }
+        }
+
+    }
+}
+}, 500);
+
+
+
+//answer
+function answer(){
+    if(level=="easy"){
+        for(var i=0;i<81;i++){
+            document.getElementById((i+1).toString()).value=easy[choosen][i];
+        }
+    }
+    else if(level=="medium"){
+        for(var i=0;i<81;i++){
+            document.getElementById((i+1).toString()).value=medium[choosen][i];
+        }
+    }
+    else if(level=="hard"){
+        for(var i=0;i<81;i++){
+            document.getElementById((i+1).toString()).value=hard[choosen][i];
+        }
+    }
+    else{
+        alert("first choose the game and start it !!");
+    }
+}
+//new game
+
+function replay(){
+    for(var i=0;i<81;i++){
+        document.getElementById((i+1).toString()).value='';
+    }
+    start();
+}
+
+
+//timer
+function timer(){
+if(document.getElementById("time1").checked==true){
+    document.getElementById("time_min").innerHTML="0"+(document.getElementById("time1_min").innerHTML-1).toString();
+    document.getElementById("time_sec").innerHTML='59';
+}
+
+else if(document.getElementById("time2").checked==true){
+    document.getElementById("time_min").innerHTML="0"+(document.getElementById("time2_min").innerHTML-1).toString();
+    document.getElementById("time_sec").innerHTML='59';
+}
+else{
+    document.getElementById("time_min").innerHTML="0"+(document.getElementById("time3_min").innerHTML-1).toString();
+    document.getElementById("time_sec").innerHTML='59';
+}
+
+    setInterval(() => {
+        if(document.getElementById("time_sec").innerHTML=='00'){
+            document.getElementById("time_sec").innerHTML="59";
+        }
+        else{
+            if(parseInt(document.getElementById("time_sec").innerHTML)<=10){
+            document.getElementById("time_sec").innerHTML="0"+(document.getElementById("time_sec").innerHTML-1).toString();
+            }
+            else{
+                document.getElementById("time_sec").innerHTML=document.getElementById("time_sec").innerHTML-1;
+            }
+        }
+    }, 1000);
+
+
+    setInterval(() => {
+        if(document.getElementById("time_min").innerHTML=='00'){
+            document.getElementById("time_sec").innerHTML='00';
+            setTimeout(() => {
+                alert("you lost !!");
+            }, 50);
+        }
+        else{
+            if(parseInt(document.getElementById("time_min").innerHTML)<=10){
+            document.getElementById("time_min").innerHTML="0"+(document.getElementById("time_min").innerHTML-1).toString();
+            }
+            else{
+                document.getElementById("time_min").innerHTML=document.getElementById("time_min").innerHTML-1;
+            }
+        }
+    }, 60*1000);
+
+}
